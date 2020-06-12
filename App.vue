3e77b939b0080b3b5d5e6e659e004e54adcc9c8a
gaol <331319344@qq.com>
2020-06-12 22:02:06 +0800

    update

--- App.vue
+++ App.vue
@@ -17,7 +17,7 @@
     /* #ifndef APP-PLUS-NVUE */
     /* uni.css - 通用组件、模板样式库，可以当作一套ui库应用 */
 	@import './assets/css/uni.css';	
-	// @import "./assets/css/index.styl";
+	@import "./assets/css/index.css";
 	@import "uview-ui/index.scss"
 	/*#endif*/
 </style>
